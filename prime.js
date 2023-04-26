d176f6780b9c85ec9a37c7c95252380f4332b70e
RaviTheja1996 <kopparapuraviteja@gmail.com>
2023-04-26 21:17:52 +0530

    prime program

---
+++ prime.js
@@ -0,0 +1,21 @@
+for(let i = 2; i <= 10; i++)
+{
+  let flag = false;
+  for(let j = 1; j <= i; j++)
+    {
+      if(i%j === 1 && j !== 1 && i !== j)
+      {
+        flag = false;
+        break;
+      }
+      else
+      {
+        flag = true;
+      }
+    }
+  if(flag)
+  {
+    console.log(i);
+  }
+}
+
